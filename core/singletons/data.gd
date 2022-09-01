80434ae61a3995e672f93ad2dd3103426c519c3f
MrBBBaiXue <mrbbbaixue@bakneko.com>
2022-09-01 14:09:38 +0800

    Init transition

--- core/singletons/data.gd
+++ core/singletons/data.gd
@@ -6,13 +6,14 @@ extends Node
 const MODULE_NAME = "GlobalData"
 
 # Scene Configs --------------------------
-const SPLASH_SCENE_PATH	: String	= "res://core/ui/splash/splash.tscn"
-const MAIN_SCENE_PATH	: String	= "res://template/maps/placeholder/placeholder.tscn"
+const SPLASH_SCENE_PATH : String = "res://core/ui/splash/splash.tscn"
+const MAIN_SCENE_PATH : String = "res://template/maps/placeholder/placeholder.tscn"
+const TRANSITION_PATH : String = "res://core/ui/transition/transition.tscn"
 
 # Resource & Packages --------------------
-var LOADER_WAIT_TIME	: float		= 0.5
-var PACKAGE_ROOT		: String	= OS.get_executable_path().get_base_dir()
-var PACKAGE_PATHS		: Array		= [ 
+var LOADER_WAIT_TIME : float = 0.5
+var PACKAGE_ROOT : String = OS.get_executable_path().get_base_dir()
+var PACKAGE_PATHS : Array = [ 
 	PACKAGE_ROOT.path_join("contents"),
 	PACKAGE_ROOT.path_join("patches"),
 ]
--- core/ui/transition/transition.gd
+++ core/ui/transition/transition.gd
@@ -1,13 +1,14 @@
 # ----------------------------------------
-# placeholder.gd
+# transition.gd
 # ----------------------------------------
-# Logic code of 'placeholder.tscn'
-extends Node2D
-const MODULE_NAME = "PlaceHolder"
+# Logic code of 'transition.tscn'
+extends Control
+const MODULE_NAME = "Transition"
 
 var scene_signal : Utils.SceneSignal = Utils.SceneSignal.new()
 
 
 # Entrypoint -----------------------------
 func _ready():
+	
 	pass
